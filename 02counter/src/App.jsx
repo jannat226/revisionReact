70294d4b8c569cf6a0e65b20cb5d4f4c400205be
Jannat226 <jannat@mca.christuniversity.in>
2025-02-06 21:03:23 -0500

    recalled how to use State just by simple example of add or remove

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,11 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // let counter = 5
+  let [counter,setCounter] = useState(5)
+  
+  const addValue =()=>{
+    counter =counter+ 1
+    setCounter(counter)
+    // or setCounter(counter +1)
+  }
+  const removeValue=()=>{
+    setCounter(counter-1)
+  }
 
   return (
     <>
-      
+      <h1>Chai aur react</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>Add value:{counter}</button>
+      <br />
+      <br/>
+      <button onClick={removeValue}>Remove Value:{counter}</button>
     </>
   )
 }
